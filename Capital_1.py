89a1f4793468325b8f585fb445a71ef860a441fd
Alina Sigdel <sigdelalina@gmail.com>
2022-08-25 22:13:30 -0400

    hi

---
+++ Capital_1.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+  transactions = [
+    ["PURCHASE","Starbucks",100,"02152022"],
+    ["PURCHASE","Amazon",20,"02152022"],
+    ["REFUND","Starbucks",100,"02192022"],
+    ["PURCHASE","Discord",70,"02162022"]
+   ]
+
+  cashback = {"Starbucks" : 0.1, "Amazon": 0.2,"Discord": 0.3}
+
+  
